359aee77e4f8ffc82d581df1a5c10176f8074f36
Christos Grigoriadis <chrisgregoriadis@hotmail.com>
2024-08-10 01:33:48 +0300

    Update start_triton.sh
    script to start triton container

--- Triton-Inference-Server/start_triton.sh
+++ Triton-Inference-Server/start_triton.sh
@@ -2,10 +2,10 @@ podman run --rm -it --net host --ulimit memlock=-1 --ulimit stack=67108864  \
                 --security-opt=label=disable --security-opt seccomp=unconfined \
                 --gpus=all --device nvidia.com/gpu=all \
                 --ipc=host \
-                --name triton_08_08 \
+                --name triton_1008 \
                 --tmpfs /tmp:exec \
                 -v ./mistral_nemo/Mistral-Nemo-Instruct-2407:/models/mistral-nemo \
                 -v ./mistral_nemo/engines:/engines \
-                -v ./mistral-nemo-trt/cudaDev/tensorrtllm_backend:/trtback \
+                -v ./tensorrtllm_backend:/trtback \
                 --user root \
-                nvcr.io/nvidia/tritonserver:24.07-trtllm-python-py3
+                triton_10_08 /bin/bash
